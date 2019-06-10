a0fcf6d1f329665661832dab86e2d55bcc52447c
kdhartmann <kaydee.hartmann@utexas.edu>
2019-06-10 14:33:11 -0500

    Graphs for options

--- app/routes.py
+++ app/routes.py
@@ -37,6 +37,7 @@ def monthly_data():
 
     return fatal_month_json
 
+# original
 @app.route('/fatality_type')
 def fatality_type():
     filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
@@ -52,6 +53,41 @@ def fatality_type():
 
     return fatal_type_json
 
+@app.route('/fatality_type_male')
+def fatality_type_male():
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    fatal_type = pd.read_csv(filename, sep=',')
+    fatal_type = fatal_type[['Fatal_Casualty_Type', 'Fatal_Accident_Index', 'Fatal_Casualty_Sex']]
+    fatal_type = fatal_type.loc[fatal_type['Fatal_Casualty_Sex'] == 'Male']
+    fatal_type = fatal_type.groupby('Fatal_Casualty_Type').agg('count')
+    fatal_type = fatal_type.rename(index=str, columns={"Fatal_Accident_Index": "Count"})
+    fatal_type = fatal_type.sort_values(by=['Count'], ascending=False)
+    fatal_type = fatal_type.reset_index()
+    fatal_type = fatal_type[['Fatal_Casualty_Type', 'Count']]
+    fatal_type['Fatal_Casualty_Type'] = fatal_type.Fatal_Casualty_Type.astype(str)
+    fatal_type = fatal_type.replace('_', ' ', regex=True)
+    fatal_type_json = fatal_type.to_json(orient='records')
+
+    return fatal_type_json
+
+@app.route('/fatality_type_female')
+def fatality_type_female():
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    fatal_type = pd.read_csv(filename, sep=',')
+    fatal_type = fatal_type[['Fatal_Casualty_Type', 'Fatal_Accident_Index', 'Fatal_Casualty_Sex']]
+    fatal_type = fatal_type.loc[fatal_type['Fatal_Casualty_Sex'] == 'Female']
+    fatal_type = fatal_type.groupby('Fatal_Casualty_Type').agg('count')
+    fatal_type = fatal_type.rename(index=str, columns={"Fatal_Accident_Index": "Count"})
+    fatal_type = fatal_type.sort_values(by=['Count'], ascending=False)
+    fatal_type = fatal_type.reset_index()
+    fatal_type = fatal_type[['Fatal_Casualty_Type', 'Count']]
+    fatal_type['Fatal_Casualty_Type'] = fatal_type.Fatal_Casualty_Type.astype(str)
+    fatal_type = fatal_type.replace('_', ' ', regex=True)
+    fatal_type_json = fatal_type.to_json(orient='records')
+
+    return fatal_type_json
+
+# original 
 @app.route('/age_fatal_count')
 def age_fatal_count():
     filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
@@ -68,6 +104,44 @@ def age_fatal_count():
 
     return age_json
 
+# new
+@app.route('/age_fatal_count_female')
+def age_fatal_count_female():
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    age = pd.read_csv(filename, sep=',')
+    age = age[['Fatal_Accident_Index', 'Fatal_Casualty_Age', 'Fatal_Casualty_Sex']]
+    age = age.loc[age['Fatal_Casualty_Sex'] == 'Female']
+    age = age.groupby('Fatal_Casualty_Age').agg('count')
+    age = age.reset_index()
+    age = age.rename(index=str, columns={"Fatal_Accident_Index": "Count"})
+    age = age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    age.drop(age.tail(1).index,inplace=True)
+    age = age[['Fatal_Casualty_Age', 'Count']]
+    age['Fatal_Casualty_Age'] = age.Fatal_Casualty_Age.astype(int)
+    age = age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    age_json = age.to_json(orient='records')
+
+    return age_json
+
+@app.route('/age_fatal_count_male')
+def age_fatal_count_male():
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    age = pd.read_csv(filename, sep=',')
+    age = age[['Fatal_Accident_Index', 'Fatal_Casualty_Age', 'Fatal_Casualty_Sex']]
+    age = age.loc[age['Fatal_Casualty_Sex'] == 'Male']
+    age = age.groupby('Fatal_Casualty_Age').agg('count')
+    age = age.reset_index()
+    age = age.rename(index=str, columns={"Fatal_Accident_Index": "Count"})
+    age = age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    age.drop(age.tail(1).index,inplace=True)
+    age = age[['Fatal_Casualty_Age', 'Count']]
+    age['Fatal_Casualty_Age'] = age.Fatal_Casualty_Age.astype(int)
+    age = age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    age_json = age.to_json(orient='records')
+
+    return age_json
+
+# original
 @app.route('/fatality_type_by_age')
 def fatality_type_by_age():
 
@@ -85,6 +159,53 @@ def fatality_type_by_age():
 
     return fatal_age_json
 
+@app.route('/fatality_type_by_age_male')
+def fatality_type_by_age_male():
+
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    fatal_age = pd.read_csv(filename, sep=',')
+    fatal_age = fatal_age[['Fatal_Casualty_Type', 'Fatal_Casualty_Age', 'Fatal_Casualty_Sex']]
+    fatal_age = fatal_age.loc[fatal_age['Fatal_Casualty_Sex'] == 'Male']
+    fatal_age = fatal_age.groupby('Fatal_Casualty_Age').agg('max')
+    fatal_age = fatal_age.reset_index()
+    fatal_age = fatal_age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    fatal_age.drop(fatal_age.tail(1).index,inplace=True)
+    fatal_age = fatal_age[['Fatal_Casualty_Age', 'Fatal_Casualty_Type']]
+    fatal_age['Fatal_Casualty_Age'] = fatal_age.Fatal_Casualty_Age.astype(int)
+    fatal_age = fatal_age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    fatal_age = fatal_age.replace('_', ' ', regex=True)
+    fatal_age_json = fatal_age.to_json(orient='records')
+
+    return fatal_age_json
+
+@app.route('/fatality_type_by_age_female')
+def fatality_type_by_age_female():
+
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    fatal_age = pd.read_csv(filename, sep=',')
+    fatal_age = fatal_age[['Fatal_Casualty_Type', 'Fatal_Casualty_Age', 'Fatal_Casualty_Sex']]
+    fatal_age = fatal_age.loc[fatal_age['Fatal_Casualty_Sex'] == 'Female']
+    fatal_age = fatal_age.groupby('Fatal_Casualty_Age').agg('max')
+    fatal_age = fatal_age.reset_index()
+    fatal_age = fatal_age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    fatal_age.drop(fatal_age.tail(1).index,inplace=True)
+    fatal_age = fatal_age[['Fatal_Casualty_Age', 'Fatal_Casualty_Type']]
+    fatal_age['Fatal_Casualty_Age'] = fatal_age.Fatal_Casualty_Age.astype(int)
+    fatal_age = fatal_age.sort_values(by=['Fatal_Casualty_Age'], ascending=True)
+    fatal_age = fatal_age.replace('_', ' ', regex=True)
+    fatal_age_json = fatal_age.to_json(orient='records')
+
+    return fatal_age_json
+
+# not used
+@app.route('/fatal_casualty')
+def fatal_casualty():
+    filename = "https://raw.githubusercontent.com/kdhartmann/Project1/master/Fatal%20Casualty.csv"
+    fatal_accident = pd.read_csv(filename, sep=',')
+    fatal_accident['Fatal_Casualty_Type'] = fatal_accident.Fatal_Casualty_Type.astype(str)
+    fatal_accident = fatal_accident.replace('_', ' ', regex=True)
+    fatal_accident_json = fatal_accident.to_json(orient='records')
+    return fatal_accident_json 
 
 ### Templates
 @app.route('/')
--- app/static/js/age_fatal_count.js
+++ app/static/js/age_fatal_count.js
@@ -1,15 +1,85 @@
+var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+//// FATALITY COUNT BY AGE: SVG1
+var margin1 = {top: 20, right: 20, bottom: 60, left: 50},
+    width1 = 1000 - margin1.left - margin1.right,
+    height1 = 250 - margin1.top - margin1.bottom;
+
+var x1 = d3.scaleBand().rangeRound([0, width1]).padding(.1);
+var y1 = d3.scaleLinear().rangeRound([height1, 0]);
+
+var svg1 = d3.select("body").append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom)
+    .append("g")
+    .attr("transform","translate(" + margin1.left + "," + margin1.top + ")");
+
+
+//// FATALITY TYPE BY AGE: SVG2
+
+var margin2 = {top: 20, right: 20, bottom: 30, left: 60},
+    width2 = 1000 - margin2.left - margin2.right,
+    height2 = 250 - margin2.top - margin2.bottom;
+
+var x2 = d3.scaleLinear().range([0, width2]);
+var y2 = d3.scalePoint().rangeRound([height2, 0]);
+
+var svg2 = d3.select("body").append("svg")
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
+    .append("g")
+    .attr("transform","translate(" + margin2.left + "," + margin2.top + ")");
+
+
+//// FATALITY TYPE BY COUNT
+
+var margin3 = {top: 20, right: 20, bottom: 35, left: 50},
+    width3 = 700 - margin3.left - margin3.right,
+    height3 = 250 - margin3.top - margin3.bottom;
+
+var x3 = d3.scaleBand().rangeRound([0, width3]).padding(.1);
+var y3 = d3.scaleLinear().rangeRound([height3, 0]);
+
+var svg3 = d3.select("body").append("svg")
+    .attr("width", width3 + margin3.left + margin3.right)
+    .attr("height", height3 + margin3.top + margin3.bottom)
+    .append("g")
+    .attr("transform","translate(" + margin3.left + "," + margin3.top + ")");
+
+
+//// BUTTON/GRAPH UPDATES
 const buttons = d3.selectAll('input');
 let selection;
 buttons.on('change', function(d) {
   selection = this.value;
-  console.log('button changed to ' + this.value);
+  console.log('button changed to ' + selection);
+
+  updateAgeFatalCount(selection);
+  updateFatalTypeByAge(selection);
+  updateFatalityType(selection);
+
 });
 
 
+//// FATALITY COUNT BY AGE 
 
-async function renderAgeFatalCount(){
+async function updateAgeFatalCount(selection){
 
-  const data = await d3.json("age_fatal_count");
+    if (selection==="Male"){
+      data = await d3.json("age_fatal_count_male");
+      titleHeight = 270;
+      fill = "steelblue";
+      console.log("selection is equal to male");
+  } else if (selection==="Female"){
+      data = await d3.json("age_fatal_count_female");
+      titleHeight = 75;
+      fill = "indianred";
+      console.log("selection is equal to female");
+  } else{
+      data = await d3.json("age_fatal_count");
+      titleHeight = 340;
+      fill = "mediumseagreen";
+  }
 
   data.forEach(function(d) {
     d.Fatal_Casualty_Age = +d.Fatal_Casualty_Age;
@@ -17,68 +87,278 @@ async function renderAgeFatalCount(){
 
   });
 
-  var margin = {top: 20, right: 20, bottom: 60, left: 50},
-    width = 1000 - margin.left - margin.right,
-    height = 250 - margin.top - margin.bottom;
+  x1.domain(data.map(d => d.Fatal_Casualty_Age));
+  y1.domain([0, d3.max(data.map(d => d.Count))]);
 
-  var x = d3.scaleBand().rangeRound([0, width]).padding(.1);
-  //https://d3-wiki.readthedocs.io/zh_CN/master/Ordinal-Scales/
-  //var x = d3.scale.ordinal().domain(data.map(d => d.Fatal_Casualty_Type)).rangeRoundBands([0, width]);
-  //var x = d3.scale.ordinal().rangeRoundBands([0, width]);
-  var y = d3.scaleLinear().rangeRound([height, 0]);
 
-  x.domain(data.map(d => d.Fatal_Casualty_Age));
-  y.domain([0, d3.max(data.map(d => d.Count))]);
-  //x.domain(data.map(function(d) {return d.Fatal_Casualty_Type; }));
-  //y.domain([0, d3.max(data, function(d){return d.Count;} )]);
-
-  var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
-
-  var tooltip = d3.select("body").append("div").attr("class", "toolTip");
-
-  // [data] doesn't allow any bars to show up
-  svg.selectAll("bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function (d) {return x(d.Fatal_Casualty_Age);})
-      .attr("y", function (d) {return y(d.Count);})
-      .attr("width", x.bandwidth())
-      .attr("height", function (d) {return height - y(d.Count);})
-      .on("mousemove", function(d){
-            tooltip
-              .style("left", d3.event.pageX - 50 + "px")
-              .style("top", d3.event.pageY - 70 + "px")
-              .style("display", "inline-block")
-              .html("Age: " + (d.Fatal_Casualty_Age) + "<br>Count: " + (d.Count));})
-      .on("mouseout", function(d){ tooltip.style("display", "none");});
-
-  // Add the X Axis
-  svg.append("g")
+//doesn't come up at all if I use .enter() for title, x and y axis 
+    // Add the X Axis
+  xaxis = svg1.append("g")
     .attr("class", "xaxis axis")
-    .attr("transform", "translate(0," + height  + ")")
-    .call(d3.axisBottom(x)
+    .attr("transform", "translate(0," + height1  + ")")
+    .call(d3.axisBottom(x1)
           .tickValues([0,10,20,30,40,50,60,70,80,90]))
     .selectAll(".tick text")
       .style("font-size", "9px");
 
-  // Add the Y Axis
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  xaxis.exit().remove();
 
-  // title
-  svg.append("text")
-    .attr("transform", "translate("+(0)+","+y(350)+")")
+  // Add the Y Axis
+  yaxis = svg1.append("g")
+    .call(d3.axisLeft(y1));
+
+  yaxis.exit().remove();
+
+   // title
+  title = svg1.append("text")
+    .attr("transform", "translate("+(0)+","+y1(titleHeight)+")")
     .attr("dy", ".35em")
     .attr("font-size", "12px")
     .attr("font-family", "arial")
     .attr("text-anchor", "start")
     .style("fill", "black")
-    .text("Fatal Casualty Count by Age");
+    .text("Fatal Casualty Count by Age for " + selection);
+
+  title.exit().remove();
+
+  group = svg1.selectAll("bar")
+    .data(data)
+    .enter().append("rect");
+
+  group.exit().remove();
+
+  group
+        .transition().duration(500)
+        .attr("class", "bar")
+        .attr("x", function (d) {return x1(d.Fatal_Casualty_Age);})
+        .attr("y", function (d) {return y1(d.Count);})
+        .attr("width", x1.bandwidth())
+        .attr("height", function (d) {return height1 - y1(d.Count);})
+        .style("fill", fill)
+        .on("mousemove", function(d){
+            d3.select(this).style("fill", "rosybrown");
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html("Age: " + (d.Fatal_Casualty_Age) + "<br>Count: " + (d.Count));})
+        .on("mouseout", function(d){ 
+          d3.select(this).style("fill", fill); 
+          tooltip.style("display", "none");});
+
+} 
+
+//// FATALITY TYPE BY AGE
+
+async function updateFatalTypeByAge(selection){
+
+  if (selection==="Male"){
+      data = await d3.json("fatality_type_by_age_male");
+      titleHeight = 350;
+      console.log("selection is equal to male");
+  } else if (selection==="Female"){
+      data = await d3.json("fatality_type_by_age_female");
+      titleHeight = 350;
+      console.log("selection is equal to female");
+  } else{
+      data = await d3.json("fatality_type_by_age");
+      titleHeight = 350;
+  }
+
+  data.forEach(function(d) {
+    d.Fatal_Casualty_Age = +d.Fatal_Casualty_Age;
+    d.Fatal_Casualty_Type = d.Fatal_Casualty_Type;
+
+  });
+  
+  x2.domain(d3.extent(data.map(d => d.Fatal_Casualty_Age)));
+  y2.domain(data.map(d => d.Fatal_Casualty_Type));
+
+  svg2.append("g")
+    .attr("class", "xaxis axis")
+    .attr("transform", "translate(0," + (height2 ) + ")")
+    .call(d3.axisBottom(x2))
+    .selectAll(".tick text")
+      .style("font-size", "9px");
+
+  svg2.append("g")
+    .attr("class", "xaxis axis")
+    .call(d3.axisLeft(y2))
+    .selectAll(".tick text")
+       .style("font-size", "8px")
+       .call(wrap2, 50);
+  
+  var cValue = function(d) { 
+    if (d==="Van Driver"){
+      color = "mediumseagreen";
+    } else if (d==="Van Passenger"){
+      color = "indianred";
+    }else if (d==="Pedestrian"){
+      color = "steelblue";
+    } else if (d==="Car Passenger"){
+      color = "blue";
+    }else{
+      color = "black";
+    }
+    return color;};
+
+  svg2.append("text")
+    .attr("transform", "translate("+(2)+","+y2(titleHeight)+")")
+    .attr("dy", ".35em")
+    .attr("font-size", "12px")
+    .attr("font-family", "arial")
+    .attr("text-anchor", "start")
+    .style("fill", "black")
+    .text("Most Frequent Fatality Type by Age for " + selection);
+
+  group2 = svg2.selectAll(".bar")
+    .data(data)
+    .enter().append("circle");
+
+  group2.exit().remove();
+
+
+  group2.transition().duration(500)
+      .attr("class", "bar")
+      .attr("cx", function(d) { return x2(d.Fatal_Casualty_Age); })
+      .attr("cy", function(d) { return y2(d.Fatal_Casualty_Type); })
+      .attr('r', 3)
+      .style("fill", function(d) { return cValue(d.Fatal_Casualty_Type);})
+      .on("mousemove", function(d){
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html("Age: " + (d.Fatal_Casualty_Age) + "<br>Type: " + (d.Fatal_Casualty_Type));})
+      .on("mouseout", function(d){ tooltip.style("display", "none");});
 
 }
-renderAgeFatalCount()
+
+
+//// FATALITY TYPE BY COUNT
+
+async function updateFatalityType(selection){
+
+  if (selection==="Male"){
+      data = await d3.json("fatality_type_male");
+      titleHeight = 2300;
+      fill = "steelblue";
+      console.log("selection is equal to male");
+  } else if (selection==="Female"){
+      data = await d3.json("fatality_type_female");
+      titleHeight = 690;
+      fill = "indianred";
+      console.log("selection is equal to female");
+  } else{
+      data = await d3.json("fatality_type");
+      titleHeight = 3000;
+      fill = "mediumseagreen"
+  }
+
+    data.forEach(function(d) {
+      d.Fatal_Casualty_Type = d.Fatal_Casualty_Type;
+      d.Count = +d.Count;
+  });
+
+  x3.domain(data.map(d => d.Fatal_Casualty_Type));
+  y3.domain([0, d3.max(data.map(d => d.Count))]);
+
+  // Add the X Axis
+  svg3.append("g")
+    .attr("class", "xaxis axis")
+    .attr("transform", "translate(0," + (height3 ) + ")")
+    .call(d3.axisBottom(x3))
+    .selectAll(".tick text")
+      .style("font-size", "8px")
+      .call(wrap3, x3.bandwidth());
+
+
+  // Add the Y Axis
+  svg3.append("g")
+    .call(d3.axisLeft(y3));
+
+  // title
+  svg3.append("text")
+    .attr("transform", "translate("+(0)+","+y3(titleHeight)+")")
+    .attr("dy", ".35em")
+    .attr("font-size", "12px")
+    .attr("font-family", "arial")
+    .attr("text-anchor", "start")
+    .style("fill", "black")
+    .text("Fatal Casualty Type by Count for " + selection);
+
+  group3 = svg3.selectAll("bar")
+    .data(data)
+    .enter().append("rect");
+
+  group3.exit().remove();
+
+  group3.transition().duration(500)
+      .attr("class", "bar")
+      .attr("x", function (d) {return x3(d.Fatal_Casualty_Type);})
+      .attr("y", function (d) {return y3(d.Count);})
+      .attr("width", x3.bandwidth())
+      .attr("height", function (d) {return height3 - y3(d.Count);})
+      .style("fill", fill)
+      .on("mousemove", function(d){
+            d3.select(this).style("fill", "rosybrown");
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html(("Type: " + d.Fatal_Casualty_Type) + "<br>Count: " + (d.Count));})
+      .on("mouseout", function(d){ 
+          d3.select(this).style("fill", fill);
+          tooltip.style("display", "none");});
+
+}
+
+
+function wrap2(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", -10).attr("y", y).attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", -10).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+  }
+
+
+  function wrap3(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+  }
